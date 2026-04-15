df9b640ecc1eccebcbc3f43ed56756b4be5c059c
JevanAbiellie <jevanabiellie@gmail.com>
2026-04-15 08:52:55 +0700

    First commit

--- soal2.c
+++ soal2.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+void rekursi(int n, int *total){
+    if (n == 1){
+        return;
+    }
+    else if (n != 1){
+        if (n%2 == 0){
+            n = n/2;
+        }
+        else if (n%2 == 1){
+            n = 3*n +1;
+        }   
+    *total += 1;
+    }
+    rekursi(n, total);
+}
+
+int main(){
+    int n,num,last;
+    scanf("%d ", &n);
+    int arr[n];
+    for (int i = 0; i < n-1; i++){
+        scanf("%d ", &num);
+        int total = 0;
+        rekursi(num, &total);
+        arr[i] = total;
+    }
+    scanf("%d", &last);
+    arr[n] = 0;
+    rekursi(last, &arr[n]);
+    for (int i = 0; i < n-1; i++){
+        printf("LANGKAH %d ", arr[i]);
+    }
+    printf("LANGKAH %d", arr[n]);
 }
